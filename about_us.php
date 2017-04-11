2dee1a7c204958d767c995c4190310959521d9e5
001343 <001343@ITDEV17.unionb.com>
2017-04-11 16:32:05 +0530

    initial project

--- about_us.php
+++ about_us.php
@@ -12,6 +12,24 @@ Licence URI: http://www.os-templates.com/template-terms
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <link rel="stylesheet" href="layout/styles/layout.css" type="text/css" />
 <link rel="stylesheet" href="css/sliate-style.css" type="text/css" />
+
+
+<style type="text/css">
+#leftContent{
+	width: 70%;
+	float: left;
+	 text-align: justify;
+    text-justify: inter-word;
+	}
+#rightContent{
+	width: 28%;
+	float: right;
+	height: 500px;
+	}
+</style>
+
+
+
 </head>
 <body id="top">
 <div class="wrapper row1">
@@ -27,11 +45,60 @@ Licence URI: http://www.os-templates.com/template-terms
 <!-- ####################################################################################################### -->
 <div class="wrapper row4">
   <div id="container" class="clear"> 
+  <div id="leftContent">
+    <h1>Vision</h1>
+    <p>To become the Centre of Excellence in Technological Education in Sri Lanka with a Wide Spread Network Covering All Provinces</p>
+     <h1>Mission</h1>
+        <p>Creating Excellent Higher National Diploma Holders with Cutting Edge State of the Art Skills in Their Chosen Fields with the Strength to be Job Takes and Not Job Seekers</p>
+        <h1>Introduction</h1>
+      <p>Sri Lanka Institute of Advanced Technological Education (SLIATE) was established under the provisions of the parliamentary act of No. 29 of 1995 under the Ministry of Higher Education. SLIATE operates 20 Institutes including 12 Advanced Technological Institutes and 08 Advanced Technological Institute Section. There are 14 Higher National Diploma Courses in various disciplines conducted in them. <br><br>
+The Governing Council of SLIATE is the supreme authority decides the administrative and policy matters of the Institute and the Academic Syndicate is accountable for the academic matters. The Chief Executive Officer of SLIATE is the Director General. The Head of an Advanced Technological Institute is the Director and the Academic Coordinator is the Head of an Advanced Technological Institute Section. <br><br>
+The development of the SLIATE in both physical and human resources during the last 20 years is remarkable. New Institutes were commenced and new courses were introduced. The whole effort of SLIATE is to create knowledgeable, skillful diploma holders with healthy attitudes based on the mission.<br><br>
+The prime objective of this “Student Handbook” is to guide the students and make a systematic, lawful and peaceful environment in the Institutes which enables students to boost their education, creativity and talents. 
+</p>
+
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td colspan="2"><h1>Message from the Director General</h1></td>
+    </tr>
+  <tr>
+    <td width="21%"><img src="images/Director General.png" /></td>
+    <td width="79%" valign="top"><br>
+    Dr. W. Hilary E.Silva<br>
+PhD; MA (Accounting & Finance); PGD; B.Com<br><br>
+
+
+Sri Lanka Institute of Advanced Technological Education (SLIATE) is dedicated to provide quality higher education with character and this endeavor provides facilities for extracurricular activities in sports and esthetic fields.<br><br>
+ 
+I am extremely happy to state that academics, professionals and management staff offer their services to develop the curriculum, build up the skills and knowledge and to provide training to our students. With the corporation that is given by the state, I am certain that our attempt at producing quality diploma holders will be a great success.<br><br>
+ 
+Every organized society/community requires rules to regulate its relationships and SLIATE is no exception. The institute through the Registrar Student Affairs’ office has issued every new student with a copy of the Students’ Hand Book. The design of the Hand Book is user friendly. The key highlights in the Hand Book includes programme and course requirements, academic advancement, examinations issues as well as general rules and regulations governing students’ code of conduct and discipline. Every student is expected to read and understand the contents of the Students’ Hand Book so as to avoid pitfalls in life which can be very costly at times.<br><br>
+
+It is important to note that an inappropriate behavior of a few can affect the reputation of the institute, locally and beyond and potentially tarnish the value of your certificate.  We urge you to be our ambassadors wherever you are in dressing and behavior, for the good name of our institute will be of great benefit to all of us as a family.<br><br>
+
+I have no doubt that you all would make best use of the academic life and emerge as a competent human resource and fulfill the expectations of your beloved parents and the nation. May Almighty Lord shower his choicest blessings on all of you and make your stay at the institute happy and fruitful.
+
+
+</td>
+  </tr>
+</table>
+
+
+    
+
+
+
+    
+    </div><!--leftContent-->
+  <div id="rightContent">right</div>
+  
+  
+  
+  
     <!-- ####################################################################################################### -->
-    <h1>Vision, mission and objectives</h1>
-    <p>On the Insert tab, the galleries include items that are designed to coordinate with the overall look of your document. You can use these galleries to insert tables, headers, footers, lists, cover pages, and other document building blocks. When you create pictures, charts, or diagrams, they also coordinate with your current document look.</p>
-    <h2>Introduction and history</h2>
-        <p>On the Insert tab, the galleries include items that are designed to coordinate with the overall look of your document. You can use these galleries to insert tables, headers, footers, lists, cover pages, and other document building blocks. When you create pictures, charts, or diagrams, they also coordinate with your current document look.</p>
+    
+   
+        
     <h3>Headline 3 Colour and Size</h3>
     <h4>Headline 4 Colour and Size</h4>
     <h5>Headline 5 Colour and Size</h5>
