998595aa3014c4380e78c1642db33fb63a065776
001343 <001343@ITDEV17.unionb.com>
2017-04-21 13:50:41 +0530

    initial project

---
+++ footer.php
@@ -0,0 +1,74 @@
+ <!--<div class="wrapper row5">
+
+
+  <div id="footer" class="clear"> 
+   
+    <div class="foot_contact">
+      <h5>Sri Lanka Institute of Advanced Technological Education </h5>
+      <address>
+  	 
+      No 320,<br> T.B. Jaya Mawatha,<br> Colombo 10.
+      </address>
+      <ul>
+        <li><strong>Tel:</strong>0112 691307, 0112 691572</li>
+        <li><strong>Fax:</strong>0112 691307, 0112 691572</li>
+      </ul>
+    </div>
+    
+   
+    <div class="footbox">
+      <h2>Administration</h2>
+      <ul>
+        <li><a href="#">Disclaimer</a></li>
+        <li><a href="#">Accessibility</a></li>
+        <li><a href="#">Freedom of Information</a></li>
+        <li><a href="#">Website Privacy</a></li>
+        <li><a href="#">Press Release Information</a></li>
+        <li><a href="#">Annual Report</a></li>
+        <li><a href="#">Financial Statements</a></li>
+        <li><a href="#">Job Vacancies</a></li>
+        <li class="last"><a href="#">Semester Dates</a></li>
+      </ul>
+    </div>
+    <div class="footbox">
+      <h2>Link Block 2</h2>
+      <ul>
+        <li><a href="#">Schools and Colleges</a></li>
+        <li><a href="#">Student Activities</a></li>
+        <li><a href="#">Student Affairs</a></li>
+        <li><a href="#">Student Development</a></li>
+        <li><a href="#">Student Financial Services</a></li>
+        <li><a href="#">Student Group Directory</a></li>
+        <li><a href="#">Student Life</a></li>
+        <li><a href="#">International Students</a></li>
+        <li class="last"><a href="#">Global Learning</a></li>
+      </ul>
+    </div>
+    <div class="footbox last">
+      <h2>Link Block 3</h2>
+      <ul>
+        <li><a href="#">Academic Advisory</a></li>
+        <li><a href="#">Academic Assistance</a></li>
+        <li><a href="#">Academic Calendars</a></li>
+        <li><a href="#">Academics Office</a></li>
+        <li><a href="#">Administration</a></li>
+        <li><a href="#">Adult Learners</a></li>
+        <li><a href="#">Alumni Chapters</a></li>
+        <li><a href="#">Alumni Events</a></li>
+        <li class="last"><a href="#">Residential Colleges</a></li>
+      </ul>
+    </div>
+    
+    
+    
+    
+   
+  </div>-->
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper">
+  <div id="copyright" class="clear">
+    <p class="fl_left">Copyright &copy; 2017 - All Rights Reserved - <a href="sliate.ac.lk">sliate.ac.lk</a></p>
+<!--    <p class="fl_right">Template by <a target="_blank" href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p> -->
+  </div>
+</div>
